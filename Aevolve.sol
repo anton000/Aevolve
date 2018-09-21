f834f084077fbdea2b4c2acd48186f3d9002e5b8
Anthony Chua <anthony@tonyotonio.com>
2018-09-20 22:58:58 -0400

    inital commit

---
+++ Aevolve.sol
@@ -0,0 +1,139 @@
+pragma solidity ^0.4.24;
+
+import "./ERC20.sol";
+import "./roles/MinterRole.sol";
+import "./lifecycle/Pausable.sol";
+
+/**
+ * @title Aevolve Token
+ */
+contract ERC20Aevolve is ERC20, MinterRole, Pausable {
+  event MintingFinished();
+
+  bool private _mintingFinished = false;
+
+  modifier onlyBeforeMintingFinished() {
+    require(!_mintingFinished);
+    _;
+  }
+
+  /**
+   * @return true if the minting is finished.
+   */
+  function mintingFinished() public view returns(bool) {
+    return _mintingFinished;
+  }
+
+  /**
+   * @dev Function to mint tokens
+   * @param to The address that will receive the minted tokens.
+   * @param amount The amount of tokens to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
+  function mint(
+    address to,
+    uint256 amount
+  )
+    public
+    onlyMinter
+    onlyBeforeMintingFinished
+    returns (bool)
+  {
+    _mint(to, amount);
+    return true;
+  }
+
+  /**
+   * @dev Function to stop minting new tokens.
+   * @return True if the operation was successful.
+   */
+  function finishMinting()
+    public
+    onlyMinter
+    onlyBeforeMintingFinished
+    returns (bool)
+  {
+    _mintingFinished = true;
+    emit MintingFinished();
+    return true;
+  }
+	
+	function burn(uint256 value) public {
+    _burn(msg.sender, value);
+  }
+
+  /**
+   * @dev Burns a specific amount of tokens from the target address and decrements allowance
+   * @param from address The address which you want to send tokens from
+   * @param value uint256 The amount of token to be burned
+   */
+  function burnFrom(address from, uint256 value) public {
+    _burnFrom(from, value);
+  }
+
+  /**
+   * @dev Overrides ERC20._burn in order for burn and burnFrom to emit
+   * an additional Burn event.
+   */
+  function _burn(address who, uint256 value) internal {
+    super._burn(who, value);
+  }
+	
+	function transfer(
+    address to,
+    uint256 value
+  )
+    public
+    whenNotPaused
+    returns (bool)
+  {
+    return super.transfer(to, value);
+  }
+
+  function transferFrom(
+    address from,
+    address to,
+    uint256 value
+  )
+    public
+    whenNotPaused
+    returns (bool)
+  {
+    return super.transferFrom(from, to, value);
+  }
+
+  function approve(
+    address spender,
+    uint256 value
+  )
+    public
+    whenNotPaused
+    returns (bool)
+  {
+    return super.approve(spender, value);
+  }
+
+  function increaseAllowance(
+    address spender,
+    uint addedValue
+  )
+    public
+    whenNotPaused
+    returns (bool success)
+  {
+    return super.increaseAllowance(spender, addedValue);
+  }
+
+  function decreaseAllowance(
+    address spender,
+    uint subtractedValue
+  )
+    public
+    whenNotPaused
+    returns (bool success)
+  {
+    return super.decreaseAllowance(spender, subtractedValue);
+  }
+	
+	
+}
